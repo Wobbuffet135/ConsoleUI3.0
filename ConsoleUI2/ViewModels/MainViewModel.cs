c21ba12d39dac95b7a8305010dbfff0f1cc7f329
osctorand <osctorand@gmail.com>
2024-10-01 19:24:46 +0200

    lade till knapp för att ta bort från listan

--- ConsoleUI2/ViewModels/MainViewModel.cs
+++ ConsoleUI2/ViewModels/MainViewModel.cs
@@ -6,16 +6,25 @@ namespace ConsoleUI2.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    ObservableCollection<string> _gameList = new();
+[ObservableProperty]
+ObservableCollection<string> _gameList = new();
 
     [ObservableProperty]
     string _tbxInputText = "";
+
+    [ObservableProperty]
+    int _selectedGameIndex;
 
 
     [RelayCommand]
     private void AddItem()
     {
-        GameList.Add(TbxInputText);
+    GameList.Add(TbxInputText);
     }
-}
+
+    [RelayCommand]
+    private void RemoveItem()
+    {
+    GameList.RemoveAt(SelectedGameIndex);
+    }
+    }
