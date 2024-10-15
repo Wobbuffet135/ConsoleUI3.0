e0589dcd88cf54243e5bcffe1a0d4389c0954f81
osctorand <osctorand@gmail.com>
2024-10-15 10:47:11 +0200

    migrerade allt

---
+++ ConsoleUI3/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ConsoleUI3.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ConsoleUI3/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace ConsoleUI3.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
