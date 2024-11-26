b706e6f14beb85145c11e3f4cc801829b3128829
JesperFrid <elev.jesper.frid@skola.lund.se>
2024-11-26 13:11:41 +0100

    Gjort om testview bilden till en lista

--- ConsoleUI3/ViewModels/TestViewModel.cs
+++ ConsoleUI3/ViewModels/TestViewModel.cs
@@ -1,10 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
 
 namespace ConsoleUI3.ViewModels;
 
 public partial class TestViewModel : ViewModelBase
 {
+    public TestViewModel()
+    {
+        AllGamesList.Add("Resident Evil");
+        AllGamesList.Add("Resident Evil");
+        AllGamesList.Add("Resident Evil");
+        AllGamesList.Add("Resident Evil");
+        AllGamesList.Add("Resident Evil");
+        AllGamesList.Add("Resident Evil");
+        AllGamesList.Add("Resident Evil");
+        AllGamesList.Add("Resident Evil");
+    }
+
+
+    [ObservableProperty] ObservableCollection<object> _allGamesList = new();
     [ObservableProperty] private bool _animationOn = false;     
 
 }
