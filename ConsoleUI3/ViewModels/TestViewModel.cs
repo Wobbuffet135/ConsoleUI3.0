2aa312047b6dfb948dd8d202e0500a411af1f2e9
JesperFrid <elev.jesper.frid@skola.lund.se>
2024-11-27 14:22:11 +0100

    laggt till observable properties till den stora listan. Inte färdig

--- ConsoleUI3/ViewModels/TestViewModel.cs
+++ ConsoleUI3/ViewModels/TestViewModel.cs
@@ -8,6 +8,12 @@ public partial class TestViewModel : ViewModelBase
 {
     public TestViewModel()
     {
+        Game fortnite = new Game();
+        fortnite.Name = "fortnite";
+        fortnite.Rating = 4;
+        fortnite.Image = "Assets\\gear-icon-2048x2048-5lk2g86a.png";
+        AllGamesList.Add(fortnite);
+
         AllGamesList.Add("Resident Evil");
         AllGamesList.Add("Resident Evil");
         AllGamesList.Add("Resident Evil");
@@ -18,8 +24,18 @@ public partial class TestViewModel : ViewModelBase
         AllGamesList.Add("Resident Evil");
     }
 
+    public class Game
+    {
+        [ObservableProperty]
+        public string Name;
 
-    [ObservableProperty] ObservableCollection<object> _allGamesList = new();
+        [ObservableProperty]
+        public object Image;
+        [ObservableProperty]
+        public float Rating;
+
+    }
+    [ObservableProperty] ObservableCollection<Game> _allGamesList = new();
     [ObservableProperty] private bool _animationOn = false;     
 
 }
